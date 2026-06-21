bcc7f4c7841f5ec990706f2bacd86daa6608be2b
Anubhav yadav <ay8400673@gmail.com>
2026-06-21 23:04:43 +0530

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
-// (Login aur Signup import abhi hata do ya comment kar do)
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
-  )
+    <Router>
+      <Routes>
+        {/* Jab URL '/' hoga, tab Landing page dikhega */}
+        <Route path="/" element={<Landing />} />
+        
+        {/* Jab URL '/login' hoga, tab Login page dikhega */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* Jab URL '/signup' hoga, tab Signup page dikhega */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
