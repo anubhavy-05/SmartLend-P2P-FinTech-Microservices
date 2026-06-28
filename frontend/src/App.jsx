82ed4fecab06c2d51094e3d14a161a24505df9b5
Anubhav yadav <ay8400673@gmail.com>
2026-06-28 23:31:48 +0530

    update gronted in my application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import BorrowerDashboard from './components/BorrowerDashboard'; // <--- Yeh Naya Import kiya
+import BorrowerDashboard from './components/BorrowerDashboard';
+import InvestorDashboard from './components/InvestorDashboard'; // <-- Naya Import
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* Naya Route Dashboard ke liye */}
         <Route path="/dashboard/borrower" element={<BorrowerDashboard />} />
+        
+        {/* Naya route Investor Dashboard ke liye */}
+        <Route path="/dashboard/investor" element={<InvestorDashboard />} />
       </Routes>
     </Router>
   );
